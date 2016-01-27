d2021fc4c60a3f75d8533cb76e4937723cdc9e66
polbins <pvintal@gmail.com>
2016-01-27 23:29:11 +0800

    add an arbitrary User POJO

--- app/src/main/java/com/polbins/databindingsample/MainActivity.java
+++ app/src/main/java/com/polbins/databindingsample/MainActivity.java
@@ -1,13 +1,28 @@
 package com.polbins.databindingsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private User mUser;
+    private TextView mFirstNameTextView;
+    private TextView mLastNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUser = new User();
+        mUser.setFirstName("Gorgonio");
+        mUser.setLastName("Magalpoc");
+
+        mFirstNameTextView = (TextView) findViewById(R.id.first_name_text_view);
+        mLastNameTextView = (TextView) findViewById(R.id.last_name_text_view);
+
+        mFirstNameTextView.setText(mUser.getFirstName());
+        mLastNameTextView.setText(mUser.getLastName());
     }
+
 }
---
+++ app/src/main/java/com/polbins/databindingsample/User.java
@@ -0,0 +1,25 @@
+package com.polbins.databindingsample;
+
+/**
+ * Created by polbins on 27/01/2016.
+ */
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
