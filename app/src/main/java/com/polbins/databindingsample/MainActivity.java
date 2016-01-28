77aebaea2a72e24db9d39c97d4bdcfc5ee2fa2b1
polbins <pvintal@gmail.com>
2016-01-28 13:55:38 +0800

    enabled Data Binding with a simple example for class User

--- app/src/main/java/com/polbins/databindingsample/MainActivity.java
+++ app/src/main/java/com/polbins/databindingsample/MainActivity.java
@@ -1,28 +1,22 @@
 package com.polbins.databindingsample;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+
+import com.polbins.databindingsample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private User mUser;
-    private TextView mFirstNameTextView;
-    private TextView mLastNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mUser = new User();
-        mUser.setFirstName("Gorgonio");
-        mUser.setLastName("Magalpoc");
-
-        mFirstNameTextView = (TextView) findViewById(R.id.first_name_text_view);
-        mLastNameTextView = (TextView) findViewById(R.id.last_name_text_view);
-
-        mFirstNameTextView.setText(mUser.getFirstName());
-        mLastNameTextView.setText(mUser.getLastName());
+        ActivityMainBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Gorgonio", "Magalpoc");
+        binding.setUser(user);
     }
 
 }
+
+
--- app/src/main/java/com/polbins/databindingsample/User.java
+++ app/src/main/java/com/polbins/databindingsample/User.java
@@ -1,11 +1,13 @@
 package com.polbins.databindingsample;
 
-/**
- * Created by polbins on 27/01/2016.
- */
 public class User {
     private String firstName;
     private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public String getFirstName() {
         return firstName;
