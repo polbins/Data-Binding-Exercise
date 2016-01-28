460f1492fa82bf0edbb40053ae56f18a21ad3714
polbins <pvintal@gmail.com>
2016-01-28 14:48:43 +0800

    added a button and an onClick listener

--- app/src/main/java/com/polbins/databindingsample/MainActivity.java
+++ app/src/main/java/com/polbins/databindingsample/MainActivity.java
@@ -3,10 +3,19 @@ package com.polbins.databindingsample;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.polbins.databindingsample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "Clicked.", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
         User user = new User("Gorgonio", "Magalpoc");
         binding.setUser(user);
+        binding.setHandler(mOnClickListener);
     }
 
 }
